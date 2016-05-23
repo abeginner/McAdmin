e4536fd4b3dabf0ac3e975d1f78f7268ced81a18
abeginner <ac123123@>
2016-05-23 11:47:51 +0800

    21111

--- Public/public_pkg/restful.py
+++ Public/public_pkg/restful.py
@@ -8,7 +8,7 @@ CONTENT_TYPE = 'application/json'
 def index(url, application, controller, params={}, content_type=None):
     if not content_type:
         content_type = CONTENT_TYPE
-    req_url = urlparse.urljoin(urlparse.urljoin(url, application), controller)
+    req_url = urlparse.urljoin(url, application+'/'+controller)
     headers = {'content-type': content_type}
     print req_url
     return requests.get(req_url, params=params, headers=headers)
@@ -16,7 +16,7 @@ def index(url, application, controller, params={}, content_type=None):
 def show(url, application, controller, r_id, params={}, content_type=None):
     if not content_type:
         content_type = CONTENT_TYPE
-    req_url = urlparse.urljoin(urlparse.urljoin(urlparse.urljoin(url, application), controller), r_id)
+    req_url = urlparse.urljoin(url, application+'/'+controller+'/'+r_id)
     headers = {'content-type': content_type}
     print req_url
     return requests.get(req_url, params=params, headers=headers)
@@ -24,7 +24,7 @@ def show(url, application, controller, r_id, params={}, content_type=None):
 def create(url, application, controller, data={}, content_type=None):
     if not content_type:
         content_type = CONTENT_TYPE
-    req_url = urlparse.urljoin(urlparse.urljoin(url, application), controller)
+    req_url = urlparse.urljoin(url, application+'/'+controller)
     headers = {'content-type': content_type}
     print req_url
     return requests.get(req_url, data=json.dumps(data), headers=headers)
@@ -32,7 +32,7 @@ def create(url, application, controller, data={}, content_type=None):
 def update(url, application, controller, r_id, data={}, content_type=None):
     if not content_type:
         content_type = CONTENT_TYPE
-    req_url = urlparse.urljoin(urlparse.urljoin(urlparse.urljoin(url, application), controller), r_id)
+    req_url = urlparse.urljoin(url, application+'/'+controller+'/'+r_id)
     headers = {'content-type': content_type}
     print req_url
     return requests.get(req_url, data=json.dumps(data), headers=headers)
@@ -40,7 +40,7 @@ def update(url, application, controller, r_id, data={}, content_type=None):
 def delete(url, application, controller, r_id, content_type=None):
     if not content_type:
         content_type = CONTENT_TYPE
-    req_url = urlparse.urljoin(urlparse.urljoin(urlparse.urljoin(url, application), controller), r_id)
+    req_url = urlparse.urljoin(url, application+'/'+controller+'/'+r_id)
     headers = {'content-type': content_type}
     print req_url
     return requests.delete(req_url, headers=headers)
