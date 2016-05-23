e8cc581e5ef5006a1ebbdfc25254a9a019798eea
abeginner <ac123123@>
2016-05-23 11:15:17 +0800

    111111

--- Public/public_pkg/restful.py
+++ Public/public_pkg/restful.py
@@ -10,6 +10,7 @@ def index(url, application, controller, params={}, content_type=None):
         content_type = CONTENT_TYPE
     req_url = urlparse.urljoin(urlparse.urljoin(url, application), controller)
     headers = {'content-type': content_type}
+    print req_url
     return requests.get(req_url, params=params, headers=headers)
     
 def show(url, application, controller, r_id, params={}, content_type=None):
@@ -17,6 +18,7 @@ def show(url, application, controller, r_id, params={}, content_type=None):
         content_type = CONTENT_TYPE
     req_url = urlparse.urljoin(urlparse.urljoin(urlparse.urljoin(url, application), controller), r_id)
     headers = {'content-type': content_type}
+    print req_url
     return requests.get(req_url, params=params, headers=headers)
 
 def create(url, application, controller, data={}, content_type=None):
@@ -24,20 +26,23 @@ def create(url, application, controller, data={}, content_type=None):
         content_type = CONTENT_TYPE
     req_url = urlparse.urljoin(urlparse.urljoin(url, application), controller)
     headers = {'content-type': content_type}
-    return requests.get(req_url, data=data, headers=headers)
+    print req_url
+    return requests.get(req_url, data=json.dumps(data), headers=headers)
 
 def update(url, application, controller, r_id, data={}, content_type=None):
     if not content_type:
         content_type = CONTENT_TYPE
     req_url = urlparse.urljoin(urlparse.urljoin(urlparse.urljoin(url, application), controller), r_id)
     headers = {'content-type': content_type}
-    return requests.get(req_url, data=data, headers=headers)
+    print req_url
+    return requests.get(req_url, data=json.dumps(data), headers=headers)
 
 def delete(url, application, controller, r_id, content_type=None):
     if not content_type:
         content_type = CONTENT_TYPE
     req_url = urlparse.urljoin(urlparse.urljoin(urlparse.urljoin(url, application), controller), r_id)
     headers = {'content-type': content_type}
+    print req_url
     return requests.delete(req_url, headers=headers)
 
 
