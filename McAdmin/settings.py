ab59033b06a5b8fd487721898d3a382bc35cc017
abeginner <ac123123@>
2016-05-24 14:59:17 +0800

    111111

--- McAdmin/settings.py
+++ McAdmin/settings.py
@@ -57,9 +57,13 @@ WSGI_APPLICATION = 'McAdmin.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mcadmin',
+        'USER': 'mcadmin',
+        'PASSWORD': '123456',
+        'HOST': '192.168.134.2',
+        'PORT': '3306',
     }
 }
 
