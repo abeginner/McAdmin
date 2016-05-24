2e53bae0ef9fff4c2b0873298691706ab3d20cb8
abeginner <ac123123@>
2016-05-24 15:23:37 +0800

    111111

--- McAdmin/settings.py
+++ McAdmin/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'McAdmin'
+    'McAdmin.mcadmin',
 )
 
 MIDDLEWARE_CLASSES = (
