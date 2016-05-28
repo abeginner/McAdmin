4b39c4a3c3211d77c2eacf82560ae85e3717aba6
abeginner <ac123123@>
2016-05-28 18:26:30 +0800

    11111

--- McAdmin/mcadmin/cmdb_api_views.py
+++ McAdmin/mcadmin/cmdb_api_views.py
@@ -48,7 +48,7 @@ class ServerInfoView(View):
                 bizs = [biz.bussiness_fullname for biz in server.bussiness.all()]
                 ips = {ip.isp.isp_shortname:ip.ipaddress for ip in server.ipaddress_set.all()}
                 result.append({'server_code':server_code, 'asset_tag':asset_tag, 'idc':idc, 'os':os, 'tech_admin':tech_admin,
-                               'sysop_admin':sysop_admin, 'server_type':server_type, 'status':status, 'bizs':bizs})
+                               'sysop_admin':sysop_admin, 'server_type':server_type, 'status':status, 'bizs':bizs, 'ips':ips})
         response = HttpResponse(json.dumps(result), content_type='application/json')
         response.status_code = 200
         return response
