8e076720d97b0ace845a520ef84c6d5794fdfd96
abeginner <ac123123@>
2016-06-01 14:49:11 +0800

    11111

--- McAdmin/mcadmin/mcadmin_forms.py
+++ McAdmin/mcadmin/mcadmin_forms.py
@@ -6,4 +6,4 @@ from McAdmin.mcadmin.models import *
 class HostQueryForm(forms.Form):
     server_code = forms.IntegerField(label=u'server id')
     interip = forms.CharField(label=u'内网ip')
-    idc_fullname = forms.ModelChoiceField(label=u'机房', empty_label=u"选择机房", queryset=IdcMirror.objects.all().values('idc_fullname'))
+    idc_fullname = forms.CharField(label=u'机房')
