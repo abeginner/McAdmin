2bc31aaddcab55b1b6af71891e7679055d3b4394
abeginner <ac123123@>
2016-06-21 19:03:50 +0800

    111

---
+++ McAdmin/static/js/public.js
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+
+function paginatorPostCommit(page) {
+	var request = document.hostform;
+	var opt = document.createElement("textarea");        
+    opt.name = "page";        
+    opt.value = page;               
+    request.appendChild(opt);
+    request.submit();        
+    return request;
+}
+
+</script>
