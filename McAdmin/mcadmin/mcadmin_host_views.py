cc37ac438e7cc09ceeff09327509de3b640b3104
abeginner <ac123123@>
2016-06-21 17:36:02 +0800

    111

--- McAdmin/mcadmin/mcadmin_host_views.py
+++ McAdmin/mcadmin/mcadmin_host_views.py
@@ -43,6 +43,7 @@ class HostQueryView(SingleObjectMixin, ListView):
         context = super(HostQueryView, self).get_context_data(**kwargs)
         csrf_token = csrf(self.request)      
         context.update(csrf_token)
+        print self.request.POST
         form = self.form_class(initial=self.request.POST)
         context.update({'form': form })
         return context
