c5d4385eef08bef9e3a95557bb838ae2d89c1af4
abeginner <ac123123@>
2016-06-26 11:27:34 +0800

    111

--- McAdmin/mcadmin/mcadmin_forms.py
+++ McAdmin/mcadmin/mcadmin_forms.py
@@ -15,7 +15,7 @@ class BussinessQueryForm(forms.Form):
 class InstanceQueryForm(forms.Form):
     instance_code = forms.CharField(label=u'实例id')
     server_code = forms.CharField(label=u'server id')
-    host = forms.CharField(label=u'实例ip')
+    host = forms.CharField(label=u'ip')
     port = forms.CharField(label=u'端口')
     bussiness_shortname = forms.CharField(label=u'项目代号')
     bussiness_fullname = forms.CharField(label=u'项目名称')
