99af4f0744a909d12a077e8611c0877a2ffad514
abeginner <ac123123@>
2016-06-26 19:54:40 +0800

    111

--- Public/agent/controller/memcache_instance.py
+++ Public/agent/controller/memcache_instance.py
@@ -9,6 +9,15 @@ class memcache_instance(BaseController):
         self.action = 'py'
         self.script = 'memcache_instance_create.py'
         self.parameter_keys = ['host', 'port', 'max_memory', 'max_connection', 'is_bind']
+        
+    def show(self):
+        print 'memcache_instance show() is called'
+        """
+        delete the memcache instance
+        """
+        self.action = 'py'
+        self.script = 'memcache_instance_del.py'
+        self.parameter_keys = ['host', 'port' ]
 
 def get_resources():
     return memcache_instance()
