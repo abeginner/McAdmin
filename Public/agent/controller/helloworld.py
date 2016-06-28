2f967b3890e1c42cd9fb35a0511bb2cfca0f1d2d
abeginner <ac123123@>
2016-06-29 01:03:28 +0800

    v1.0

--- Public/agent/controller/helloworld.py
+++ Public/agent/controller/helloworld.py
@@ -5,7 +5,6 @@ from base import BaseController
 class helloworld(BaseController):
     
     def index(self):
-        print 'helloworld index() is called'
         self.action = 'shell'
         self.script = 'helloworld.sh'
 
