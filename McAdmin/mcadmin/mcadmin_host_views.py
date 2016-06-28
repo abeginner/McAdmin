08c7e21a4263fb047ddfef89491dde9f41d6b12b
abeginner <ac123123@>
2016-06-28 13:52:23 +0800

    1111

--- McAdmin/mcadmin/mcadmin_host_views.py
+++ McAdmin/mcadmin/mcadmin_host_views.py
@@ -129,6 +129,8 @@ class HostCreateView(View):
                 return HttpResponseRedirect("/mcadmin/host/display?msg_type=warning&msg=查询到memcache宿主机存在，且状态为" + str(host_fsm.get_status_name(status)) + ',拒绝添加.')
             mc_host.status = 0
             host_fsm.add_by_model(mc_host)
+            print '2222'
+            print host_fsm.get_status(server_info['server_code'])
         except MemcacheHost.DoesNotExist:
             server_code = server_info['server_code']
             interip = None
